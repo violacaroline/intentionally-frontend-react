884d7f57a631551461606fb1cf57b66e8a255d7b
Caroline Akesson <ca223pw@student.lnu.se>
2022-05-15 10:45:20 -0500

    Change links in nav to NavLink elements, add active ternary

--- intention-app/src/Navbar.js
+++ intention-app/src/Navbar.js
@@ -1,29 +1,28 @@
-import  { Link, useNavigate } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import useAuth from './hooks/useAuth.js'
 
 
-const Navbar = () => {  
+const Navbar = () => {
   const title = 'Intentionally'
   const { auth, setAuth } = useAuth()
   const navigate = useNavigate()
 
   const logOut = () => {
-    console.log('Auth from Navbar logout before', auth)
     setAuth()
-    
-    console.log('Auth from Navbar logout after', auth)
     navigate('/')
-    console.log('Auth from Navbar logout after redirect', auth)
-   }
+  }
 
   return (
     <nav className="navbar">
       <h1 className="heading"> {title} </h1>
       <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/moods">Our moods</Link>
-        {auth && <Link to="/practice">Practice</Link> }
-        {auth ? <button onClick={logOut}>Logout</button> : <> <Link to="/register">Register</Link> <Link to="/login">Login</Link> </>}      
+        <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
+        <NavLink to="/moods" className={({ isActive }) => (isActive ? "active" : "")}>Our moods</NavLink>
+        {auth && <NavLink to="/practice" className={({ isActive }) => (isActive ? "active" : "")}>Practice</NavLink>}
+        {auth ?
+          <button onClick={logOut}>Logout</button>
+          : <> <NavLink to="/register" className={({ isActive }) => (isActive ? "active" : "")}>Register</NavLink>
+            <NavLink to="/login" className={({ isActive }) => (isActive ? "active" : "")}>Login</NavLink> </>}
       </div>
     </nav>
   )
