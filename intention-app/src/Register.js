38c3711b9417338279043f8cbf80d4f0d12cfd25
Caroline Akesson <ca223pw@student.lnu.se>
2022-05-21 15:49:34 -0500

    Successfully validate admin, ugly solution though

--- intention-app/src/Register.js
+++ intention-app/src/Register.js
@@ -10,6 +10,7 @@ const Register = () => {
   const userRef = useRef()
   const errorRef = useRef()
 
+
   const [username, setUsername] = useState('')
   const [validName, setValidUsername] = useState(false)
 
@@ -29,7 +30,9 @@ const Register = () => {
   const [validEmail, setValidEmail] = useState(false)
 
   const [admin, setAdmin] = useState(false)
-  // const [validAdmin, setValidAdmin] = useState(false)
+  const [adminPassPhrase, setAdminPassPhrase] = useState('')
+
+  const validAdminPassPhrase = 'iamadmin'
 
   const [errMsg, setErrMsg] = useState('')
   const [success, setSuccess] = useState(false)
@@ -56,154 +59,176 @@ const Register = () => {
   }, [email])
 
   // VALIDATE ADMIN?
-//   useEffect(() => {
-//     setValidAdmin()
-// }, [admin])
+  useEffect(() => {
+    setAdminPassPhrase(adminPassPhrase)
+  }, [adminPassPhrase])
 
 
   useEffect(() => {
     setErrMsg('')
   }, [username, password, matchPassword, firstName, lastName, email, admin])
 
-const handleSubmit = async (e) => {
-  e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault()
 
-  const validUsername = nameRegex.test(username)
-  const validPassword = passwordRegex.test(password)
-  const validEmail = validator.isEmail(email)
-  if (!validUsername || !validPassword) {
-    setErrMsg("Invalid Username or Pass Phrase")
-    return
-  } else if (!validEmail) {
-    setErrMsg("Invalid Email")
-    return
-  } else if (!(password === matchPassword)) {
-    setErrMsg("Pass Phrase do not match")
-    return
-  }
-
-  try {
-    const response = await axios.post('http://localhost:8086/api/v1/register',
-      JSON.stringify({ username, password, firstName, lastName, email, admin }),
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-        // withCredentials: true  // <-- THIS WILL NEED TO BE LOOKED INTO DUE TO CORS
-      }
-    )
-    console.log(JSON.stringify(response)) // I DONT NEED TO SAVE THIS IN A VARIABLE?
-    setSuccess(true)
-
-    setUsername('')
-    setPassword('')
-    setMatchPwd('')
-    setFirstName('')
-    setLastName('')
-    setEmail('')
-    setAdmin('')
-  } catch (err) {
-    if (!err?.response) {
-      setErrMsg('No Server Response. No internet?')
-    } else if (err.response?.status === 409) {
-      setErrMsg('Username Taken')
-    } else {
-      setErrMsg('Registration Failed')
+    const validUsername = nameRegex.test(username)
+    const validPassword = passwordRegex.test(password)
+    const validEmail = validator.isEmail(email)
+    let isValidAdmin = true
+    if (admin) {
+      isValidAdmin = adminPassPhrase === validAdminPassPhrase
     }
-    errorRef.current.focus()
+
+    if (!validUsername || !validPassword) {
+      setErrMsg("Invalid Username or Pass Phrase")
+      return
+    } else if (!validEmail) {
+      setErrMsg("Invalid Email")
+      return
+    } else if (!(password === matchPassword)) {
+      setErrMsg("Pass Phrase do not match")
+      return
+    } else if (!isValidAdmin) {
+      setErrMsg("Invalid Admin Pass Phrase")
+      return
+    }
+
+    try {
+      await axios.post('http://localhost:8086/api/v1/register',
+        JSON.stringify({ username, password, firstName, lastName, email, admin }),
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+          // withCredentials: true  // <-- THIS WILL NEED TO BE LOOKED INTO DUE TO CORS
+        }
+      )
+      setSuccess(true)
+
+      setUsername('')
+      setPassword('')
+      setMatchPwd('')
+      setFirstName('')
+      setLastName('')
+      setEmail('')
+      setAdmin('')
+      setAdminPassPhrase('')
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg('No Server Response. No internet?')
+      } else if (err.response?.status === 409) {
+        setErrMsg('Username Taken')
+      } else {
+        setErrMsg('Registration Failed')
+      }
+      errorRef.current.focus()
+    }
   }
-}
 
-return (
+  return (
 
-  <section className="register">
-    <form className="input-form" onSubmit={handleSubmit}>
-      {
-        success ? (
-          <section className="success-register-login">
-            <h3 >You are Registered</h3>
-            <p>
-              <Link className="link-login" to="/login">{'Login '+ String.fromCharCode("0x00002661")}</Link>
-            </p>
-          </section >
-        ) : (
-          <>
-            <p ref={errorRef} style={{ color: "red", size: "10px" }} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-            <label htmlFor="firstName">First Name</label>
-            <input
-              type="text"
-              id="firstName"
-              autoFocus="on"
-              autoComplete="off"
-              onChange={(e) => setFirstName(e.target.value)}
-              value={firstName}
-              required
-              aria-invalid={validFirstName ? "false" : "true"}
-            />
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              type="text"
-              id="lastName"
-              autoComplete="off"
-              onChange={(e) => setLastName(e.target.value)}
-              value={lastName}
-              required
-              aria-invalid={validLastName ? "false" : "true"}
-            />
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              id="username"
-              ref={userRef}
-              autoComplete="off"
-              onChange={(e) => setUsername(e.target.value)}
-              value={username}
-              required
-              aria-invalid={validName ? "false" : "true"}
-              aria-describedby="uidnote"
-            />
-            <label htmlFor="password">Pass Phrase</label>
-            <input
-              type="password"
-              id="password"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              required
-              aria-invalid={validPassword ? "false" : "true"}
-            />
-            <label htmlFor="confirmPassword">Confirm Pass Phrase</label>
-            <input
-              type="password"
-              id="confirmPassword"
-              onChange={(e) => setMatchPwd(e.target.value)}
-              value={matchPassword}
-              required
-              aria-invalid={validMatch ? "false" : "true"}
-            />
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              id="email"
-              autoComplete="off"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              required
-              aria-invalid={validEmail ? "false" : "true"}
-            />
-            <label htmlFor="admin">Are you an admin?</label>
-            <input
-              type="checkbox"
-              className="register-checkbox"
-              id="admin"
-              onChange={(e) => setAdmin(e.target.checked)}
-              value={admin}
-            />
-            <input type="submit" value={'Submit  ' + String.fromCharCode("0x00002661")} className="submit-button" />
-          </>
-        )}
-    </form>
-  </section>
-)
+    <section className="register">
+      <form className="input-form-register" onSubmit={handleSubmit}>
+        {
+          success ? (
+            <section className="success-register-login">
+              <h3 >You are Registered</h3>
+              <p>
+                <Link className="link-login" to="/login">{'Login ' + String.fromCharCode("0x00002661")}</Link>
+              </p>
+            </section >
+          ) : (
+            <>
+              <p ref={errorRef} style={{ color: "red", size: "10px" }} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+              <label htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                id="firstName"
+                autoFocus="on"
+                autoComplete="off"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                required
+                aria-invalid={validFirstName ? "false" : "true"}
+              />
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                type="text"
+                id="lastName"
+                autoComplete="off"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                required
+                aria-invalid={validLastName ? "false" : "true"}
+              />
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                required
+                aria-invalid={validName ? "false" : "true"}
+                aria-describedby="uidnote"
+              />
+              <label htmlFor="password">Pass Phrase</label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                required
+                aria-invalid={validPassword ? "false" : "true"}
+              />
+              <label htmlFor="confirmPassword">Confirm Pass Phrase</label>
+              <input
+                type="password"
+                id="confirmPassword"
+                onChange={(e) => setMatchPwd(e.target.value)}
+                value={matchPassword}
+                required
+                aria-invalid={validMatch ? "false" : "true"}
+              />
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                id="email"
+                autoComplete="off"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                required
+                aria-invalid={validEmail ? "false" : "true"}
+              />
+              <label htmlFor="admin">Are you an admin?</label>
+              <input
+                type="checkbox"
+                className="register-checkbox"
+                id="admin"
+                onChange={(e) => setAdmin(e.target.checked)}
+                value={admin}
+              />
+              {admin ?
+                <>
+                  <label htmlFor="admin-password">Admin Pass Phrase</label>
+                  <input
+                    type="password"
+                    id="admin-password"
+                    onChange={(e) => setAdminPassPhrase(e.target.value)}
+                    required
+                  />
+                </>
+                : <>
+                  <p></p>
+                </>
+              }
+              <input type="submit" value={'Submit  ' + String.fromCharCode("0x00002661")} className="submit-button" />
+            </>
+          )}
+      </form>
+    </section >
+  )
 }
 
 export default Register
