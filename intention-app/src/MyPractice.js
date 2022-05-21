78b193d4cb56e4bff37c00ae27ca3127f37faa38
Caroline Akesson <ca223pw@student.lnu.se>
2022-05-21 17:23:02 -0500

    Add welcoming and username

--- intention-app/src/MyPractice.js
+++ intention-app/src/MyPractice.js
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom'
 
 const MyPractice = () => {
   const navigate = useNavigate()
+  const authenticatedUser = JSON.parse(localStorage.getItem("user"))
 
   const goBack = () => {
     navigate('/practice')
@@ -9,11 +10,13 @@ const MyPractice = () => {
   return (  
     <div className="my-practice">
       <button onClick={goBack}>&#10006; Go Back </button>
-      <h2>Not yet Implemented</h2>
-      <h3>Coming Soon</h3>
+      <div className="my-practice-text">
+      <h2>Hi {authenticatedUser.username +'! ' + String.fromCharCode("0x00002661")} </h2>
+      <h3>This is how you have been feeling lately</h3>
       <p>Journaling Prompts</p>
       <p>My Mood</p>
       <p>Delete User</p>
+      </div>
     </div>
   )
 }
