c5ad29c6810178b61598955914c18aa81c8858ff
Caroline Akesson <ca223pw@student.lnu.se>
2022-05-24 15:58:49 -0500

    Display the chart with fake data and give it styles

--- intention-app/src/Chart.js
+++ intention-app/src/Chart.js
@@ -1,41 +1,53 @@
-import { Pie } from "react-chartjs-2"
+import { Pie } from 'react-chartjs-2'
 import React from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend)
 
 export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  labels: ['Joy', 'Fear', 'Sadness', 'Anger', 'Disgust'],
   datasets: [
     {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      data: [4,7,10,2,6],
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
         'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
         'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
       ],
       borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
         'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
         'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(75, 192, 192, 1)',
       ],
-      borderWidth: 1,
+      borderWidth: 0.9,
     },
   ],
 }
 
 const Chart = ({ chartData }) => {
+  const pieOptions = {
+    plugins: {
+      legend: {       
+        align: 'center',
+        display: true,
+        labels: {
+          padding: 10,
+          font: {
+            size: 12,
+            family: 'Domine',
+          },
+        },
+      },
+    },
+  }
 
     return (
       <div className="chart">
-        <Pie data={data} />
+        <Pie className="pie-chart" data={data} options={pieOptions}/>
       </div>
     )
   }
--- intention-app/src/MyPractice.js
+++ intention-app/src/MyPractice.js
@@ -1,73 +1,86 @@
 import { useNavigate } from 'react-router-dom'
-// import { useState, useEffect } from 'react'
-// import axios from 'axios'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Chart from './Chart'
 
 
 
 const MyPractice = () => {
-  // const [moods, setMoods] = useState([])
-  // const [joy, setJoy] = useState([])
-  // const [fear, setFear] = useState([])
-  // const [sadness, setSadness] = useState([])
-  // const [anger, setAnger] = useState([])
-  // const [disgust, setDisgust] = useState([])
+  const [moods, setMoods] = useState([])
+  const [joy, setJoy] = useState([])
+  const [fear, setFear] = useState([])
+  const [sadness, setSadness] = useState([])
+  const [anger, setAnger] = useState([])
+  const [disgust, setDisgust] = useState([])
 
-  // const [chartData, setChartdata] = useState({})
+  const [chartData, setChartdata] = useState({})
 
 
   const navigate = useNavigate()
   const authenticatedUser = JSON.parse(localStorage.getItem("user"))
 
-  // const fetchMoods = async () => {
-  //   try {
-  //     const response = await axios.get('http://localhost:8087/api/v1/moods', {
-  //       headers: {
-  //         authorization: authenticatedUser.accessToken
-  //       }
-  //     })
+  const fetchMoods = async () => {
+    try {
+      const response = await axios.get('http://localhost:8087/api/v1/moods', {
+        headers: {
+          authorization: authenticatedUser.accessToken
+        }
+      })
 
-  //     if (moods.length === 0) {
-  //       setMoods(response.data)
-  //       console.log('Res data', response.data)
-  //     }
-  //     return response.data
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-  // const data = fetchMoods()
-  // console.log(data)
+      if (moods.length === 0) {
+        setMoods(response.data)
+        console.log('Res data', response.data)
+      }
+      // return response.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
+  fetchMoods()
+  console.log(moods)
 
+  useEffect(() => {
+    if (moods.length > 0) {
+      setJoy(moods.filter((mood) => mood.mood === 'joy'))
+      setFear(moods.filter((mood) => mood.mood === 'fear'))
+      setSadness(moods.filter((mood) => mood.mood === 'sadness'))
+      setAnger(moods.filter((mood) => mood.mood === 'anger'))
+      setDisgust(moods.filter((mood) => mood.mood === 'disgust'))
+    }
+  }, [moods])
 
-  // useEffect(() => {
-  //   fetchMoods()
-  //   console.log(moods)
+  console.log('lengths', joy.length, fear.length, sadness.length, anger.length, disgust.length)
 
-  //   if (moods) {
-  //     setJoy(moods.filter((mood) => mood.mood === 'joy'))
-  //     setFear(moods.filter((mood) => mood.mood === 'fear'))
-  //     setSadness(moods.filter((mood) => mood.mood === 'sadness'))
-  //     setAnger(moods.filter((mood) => mood.mood === 'anger'))
-  //     setDisgust(moods.filter((mood) => mood.mood === 'disgust'))
+  useEffect(() => {
+    if (moods.length > 0) {
+      setChartdata({
+        labels: ['Joy', 'Fear', 'Sadness', 'Anger', 'Disgust'],
+        datasets: [
+          {
+            data: [joy.length, fear.length, sadness.length, anger.length, disgust.length],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      })
+    }
+  }, [anger.length, disgust.length, fear.length, joy.length, moods, sadness.length])
 
-  //     console.log('Array lenghts', joy.length, fear.length, sadness.length, anger.length, disgust.length)
-  //   }
-
-
-  //   setChartdata({
-  //     labels: ['Joy', 'Fear', 'Sadness', 'Anger', 'Disgust'],
-  //     datasets: [
-  //       {
-  //         data: [joy.length, fear.length, sadness.length, anger.length, disgust.length]
-  //       }
-  //     ]
-  //   })
-
-  //   //console.log('Chart data', chartData)
-  // }, [joy.length, fear.length, sadness.length, anger.length, disgust.length, moods, authenticatedUser.accessToken, fetchMoods])
-
+  console.log('The chart data', chartData)
 
 
   const goBack = () => {
@@ -80,13 +93,14 @@ const MyPractice = () => {
       <div className="my-practice-text">
         <h2>Hi {authenticatedUser.username + '! ' + String.fromCharCode("0x00002661")} </h2>
         <h3>This is how you have been feeling lately</h3>
+        <h3>Fake data from Chart component...</h3>
         {/* {moods.map((mood, index) => (
           <div key={index} className="user-mood">
             <h4>{mood.date}</h4>
             <p>{mood.mood}</p>
           </div>
         ))} */}
-        {/* {chartData.length > 0 ? <Chart /> : <p>You have not logged any moods yet..</p>} */}
+        {/* {chartData.length > 0 ? <Chart data={chartData} /> : <p>You have not logged any moods yet..</p>} */}
         <Chart />
       </div>
     </div>
